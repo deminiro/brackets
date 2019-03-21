0aab21246e39e6d97751afa439976100c6f346a6
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-21 13:33:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var as = str.split(', ');
+    var opens = ['(','{','[','|','1','3','5','7','8'];
+    var closes = [')','{','[','|','2','4','6','7','8'];
+  var stack = [];
+  as.sort(bracketsConfig);
+  return true;
 }
