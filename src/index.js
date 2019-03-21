3609209bfeb7542d47938ee25f99acb03ab5cd72
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-21 14:19:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  var as = str.split(', ');
-    var opens = ['(','{','[','|','1','3','5','7','8'];
-    var closes = [')','{','[','|','2','4','6','7','8'];
-  var stack = [];
-  as.sort(bracketsConfig);
-  return true;
+  const devide = [['(', ')']];
+  const devide1 = [['{', '}']];
+  const devide2 = [['[', ']']];
+  const devide3 = [['|', '|']];
+  const devide4 = [['1', '2']];
+  const devide5 = [['3', '4']];
+  const devide6 = [['5', '6']];
+  const devide7 = [['7', '7']];
+  const devide8 = [['8', '8']];
+
+  let stack = [];
+  const as = str.split('');
+  if(as.length%2 === 0){
+    for(i=0;i<=as.length;i++){
+      stack.push(as[0]);
+      as.shift();
+      for(j=0;j<as.length;j++){
+        
+      }
+    }
+  }else{
+  return false;
+  }
 }
