b4609e02b0ff01ba153757fc8946c7f1672991ee
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-21 17:31:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,64 +17,109 @@ module.exports = function check(str, bracketsConfig) {
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide0[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide1[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide1[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide2[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide2[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide3[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide3[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide4[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide4[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide5[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide5[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide6[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide6[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide7[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide7[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       if(as[0] === devide8[0]){
         stack.push(as[0]);
         as.shift();
         const ind = as.indexOf(devide8[1],[fromIndex = 0]);
+        const copy = as.slice(0,ind);
+        if(copy.length % 2 !== 0){
+          return false;
+        }else{
         stack.push(as[ind]);
         as.splice(ind,1);
+        }
       }
       else if( as[0] === devide0[1] || as[0] === devide1[1] 
              ||as[0] === devide2[1] || as[0] === devide4[1]
